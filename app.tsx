e9448346490987751bbaaf9c7b46658fdddf6f6a
G .Tarun Naga Sai <22r11a6949@gcet.edu.in>
2025-09-18 12:54:51 +0530

    Add files via upload

---
+++ app.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage.tsx';
+
+const App: React.FC = () => {
+  return (
+    <HashRouter>
+      <div className="flex flex-col min-h-screen bg-slate-50 text-slate-800">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </HashRouter>
+  );
+};
+
+export default App;
